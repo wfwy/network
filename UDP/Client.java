f6f5874b16e32f000e87884616508e8a4b816f44
wfwy <1668603052@qq.com>
2018-10-24 18:39:20 +0800

    更新 Client.java

--- UDP/Client.java
+++ UDP/Client.java
@@ -1,41 +1,43 @@
- /*
-            *基于无连接的数据报通信
-            * UDP
-            * 客户端发送，服务器接收
+/*
+                 *基于无连接的数据报通信
+                 * UDP
+                 * 客户端发送，服务器接收
  */
 
-public class Server {
-
+public class Client{
     public static void main(String[] args){
-        new Server();
+       new Client();
     }
-    public Server(){                        //构造方法
-        ServerThread st=new ServerThread();
-        st.start();
+    public Client(){
+        ClientThread ct=new ClientThread();
+        ct.start();
     }
-
-    class ServerThread extends Thread{          //服务器类线程
+    class ClientThread extends Thread{          //客户端类线程
         @Override
         public void run() {
             String str;
+            String ServerName= null;
             try {
-                DatagramSocket ds=new DatagramSocket(8000);  //定义端口号
-                System.out.print("服务器名：");
-                System.out.println(InetAddress.getLocalHost().getHostName()); //显示计算机名称
+                ServerName = InetAddress.getLocalHost().getHostName();
+                System.out.println("请发送消息给服务器:"+ServerName);
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+            try {
+                DatagramSocket ds=new DatagramSocket();     //建立UDP Socket端口对象
+                DatagramPacket dp;                          //发送数据对象
                 while (true){
-                    byte[] inbf=new byte[256];
-                    DatagramPacket dp=new DatagramPacket(inbf,inbf.length);//设置接收信息
-                    ds.receive(dp);                         //接收数据报分组
-                    str=new String(dp.getData());           //将接收到的分组中的数据转化为字符串
-                    str=str.trim();                         //去掉字符串中的首位空格
-                    if (str.length()>0){
-                        int pot=dp.getPort();                //获取远程端口号
-                        System.out.println("远程端口号："+pot);
-                        System.out.println("服务器已接收信息："+str);
-                    }
+                    BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+                    System.out.println("请输入信息：");
+                    str=bf.readLine();
+                    byte[] outbf=new byte[str.length()];
+                    outbf=str.getBytes();
+                    InetAddress address=InetAddress.getByName(ServerName);
+                    dp=new DatagramPacket(outbf,outbf.length,address,8000);     //数据打包
+                    ds.send(dp);
                 }
             }catch (IOException e){
-                return;
+                System.out.println(e);
             }
         }
     }
